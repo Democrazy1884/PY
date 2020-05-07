d79ae5eea2667dfcdfe399a930c92e0a7e056e9a
democrazy <democrazy1884@gmail.com>
2020-05-07 16:31:46 +0800

    2.0

--- screen.py
+++ screen.py
@@ -2,8 +2,8 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import *
 import win32gui
 import sys
- 
-hwnd = win32gui.FindWindow(None, 'D:\program\scrcpy-win64-v1.13\scrcpy-noconsole.exe')
+
+hwnd = win32gui.FindWindow(None, 'MI 9 SE')
 app = QApplication(sys.argv)
 screen = QApplication.primaryScreen()
 img = screen.grabWindow(hwnd).toImage()
--- start.ps1
+++ start.ps1
@@ -1,5 +1,4 @@
 ﻿adb devices
-scrcpy-noconsole
 adb shell input keyevent 26
 Start-Sleep -s 0.5
 adb shell input swipe 500 1000 500 20 500
@@ -7,3 +6,5 @@ Start-Sleep -s 0.5
 adb shell input text  "132659"
 Start-Sleep -s 0.5
 adb shell am start -n jp.goodsmile.touhoulostword_android/com.google.firebase.MessagingUnityPlayerActivity
+Start-Sleep -s 3
+python screen.py
