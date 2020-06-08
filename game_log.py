64031ed99b17cbb710272336e74c68a29c5c6f89
democrazy <democrazy1884@gmail.com>
2020-06-08 10:12:42 +0800

    bug fix

--- game_log.py
+++ game_log.py
@@ -1,10 +1,9 @@
 # -*- coding:utf-8 -*-
 import logging
-from time import strftime, localtime
 
 logging.basicConfig(
     level=logging.DEBUG,  # 控制台打印的日志级别
-    filename=strftime("log\\game.log",
+    filename="log\\game.log",
     filemode="a",  # 模式，有w和a，w就是写模式，每次都会重新写日志，覆盖之前的日志
     # a是追加模式，默认如果不写的话，就是追加模式
     format="%(asctime)s - %(levelname)s: %(message)s"
